cacdba51b5d46b0abfe3a4ed5ed0e73003c138b1
denusbtw <denisrozum@gmail.com>
2025-08-08 14:22:28 +0300

    feat(analytics): create StreamingHistory model

--- spotify_stats/analytics/models.py
+++ spotify_stats/analytics/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from spotify_stats.core.models import UUIDModel, TimestampedModel
+
+
+class StreamingHistory(UUIDModel, TimestampedModel):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    track = models.ForeignKey(
+        "catalog.Track",
+        on_delete=models.CASCADE,
+        related_name="history"
+    )
+    played_at = models.DateTimeField() # 'ts'
+    ms_played = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.track.name}: {self.played_at.strftime('%Y/%m/%d, %H:%M:%S')}"
